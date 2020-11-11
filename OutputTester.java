4f6b23db452b17bceeaf51a5ce1a1d3529b4faa4
Harshit Varma <varmaharshit2@gmail.com>
2020-11-11 17:10:46 +0530

    Output tester added

---
+++ OutputTester.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class OutputTester{
+
+    public static <T> void print(T s){
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = 54;
+
+        // First word:
+        String temp = sc.nextLine().trim();
+        if (!temp.equals("WE")){
+            print("Wrong (1)");
+            return;
+        }
+        String prev = temp;
+
+        int nw = 1, nr = 0;
+
+        for (int i=1; i<n; i++){
+            temp = sc.nextLine().trim();
+
+            if (prev.equals("WE")){
+                if (!temp.equals("WX"))
+                    print("Wrong (2)");
+            }
+
+            if (temp.equals("WE"))
+                nw++;
+            else if (temp.equals("RS"))
+                nr++;
+
+            if (nr > nw)
+                print("Wrong (4)");
+
+            prev = temp;
+        }
+    }
+}
