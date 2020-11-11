a1572aa91a7dd5a46e4f0e9f74ed761c06fe38f7
devansh-dvj <devansh.dvj@gmail.com>
2020-11-11 12:19:40 +0530

    Used locks instead of synchronized

--- readwrite/ProtectedTree.java
+++ readwrite/ProtectedTree.java
@@ -1,9 +1,16 @@
 package readwrite;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProtectedTree {
   private Tree tree;
   private int writes;
   private int success_reads;
+
+  private final Lock lock = new ReentrantLock();
+  private final Condition read_check = lock.newCondition();
 
   public ProtectedTree(Tree t) {
     tree = t;
@@ -12,30 +19,30 @@ public class ProtectedTree {
   }
 
   public void write(int value) throws InterruptedException {
-    synchronized (this) {
-      System.out.println("WE");
-      this.tree.write(value);
-      System.out.println("WX");
-      writes++;
-      notify();
-    }
+    lock.lock();
+    System.out.println("WE");
+    this.tree.write(value);
+    System.out.println("WX");
+    writes++;
+    read_check.signal();
+    lock.unlock();
   }
 
   public int read(int value) throws InterruptedException {
-    synchronized (this) {
-      while (!(writes > success_reads)) {
-        wait();
-      }
-      int answer = this.tree.read(value);
-      if (answer == value)
-        System.out.println("RS");
-      else
-        System.out.println("RF");
-      if (answer == value) {
-        success_reads++;
-      }
-      notify();
-      return answer;
+    lock.lock();
+    while (!(writes > success_reads)) {
+      read_check.await();
     }
+    int answer = this.tree.read(value);
+    if (answer == value)
+      System.out.println("RS");
+    else
+      System.out.println("RF");
+    if (answer == value) {
+      success_reads++;
+    }
+    read_check.signal();
+    lock.unlock();
+    return answer;
   }
 }
